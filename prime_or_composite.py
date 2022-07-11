3842a85b126b1da9ca2d6a02c225cbada5954a7f
Пользователь <malou.1990@gmail.com>
2022-07-11 16:19:30 +0300

    add prime_or_composite.py

---
+++ prime_or_composite.py
@@ -0,0 +1,37 @@
+# This a small code defines is it number prime or composite and sort it by group
+
+my_range = int(input("The range starts from 1 and ends with the number you specify:  "))
+
+prime_nums = []
+composite_nums = []
+
+def main():
+    for num in range(2, my_range+1):
+        if is_prime(num):
+            prime_nums.append(num)
+        else:
+            composite_nums.append(num)
+    print("It's composite numbers in range", composite_nums)
+    print("It's prime numbers in range", prime_nums)
+
+
+
+def is_prime(numb):
+    step = 0
+    divider = 0
+    acum= []
+    while step < numb:
+        step += 1
+        divider += 1
+        if (numb % step == 0):
+            acum.append(divider)
+        else:
+            continue
+    if len(acum) > 2:
+        status = False
+        return status
+    else:
+        status = True
+        return status
+
+main()
